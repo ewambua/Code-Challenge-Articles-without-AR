12bcc45e3273f8b78fe109971acd7e8acda83c63
EmmanuelWambua <emmanuelwambua931@gmail.com>
2023-05-28 22:41:18 +0300

    add article and magazine files

--- lib/magazine.rb
+++ lib/magazine.rb
@@ -3,12 +3,30 @@ class magazine
     def initialize (name, category)
         @name = name
         @category = category
+        @@all << self
     end
 
-    def contributor
+
+    @@all = []
+
+    def self.all
+        @@all
+      end
+
+    def contributors
         Article.all.select { |article| article.magazine == self }.map { |article| article.author }
     end
 
-    def get_name(name)
+    def find_by_name(name)
+        @@all.find { |magazine| magazine.name == name }
+    end
 
+    def article_titles
+        Article.all.select { |article| article.magazine == self }.map { |article| article.title }
+    end
+
+    def contributing_authors
+        popular_authors = contributors.uniq
+        popular_authors.select { |author| author.articles.count > 2 }
+    end
 end
